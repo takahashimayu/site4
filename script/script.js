07fd04d9911fa1f7e976988a270d665d22dfeb2d
takahashimayu <m2bear.happy@gmail.com>
2019-09-06 15:45:12 +0900

    first commit

--- script/script.js
+++ script/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+// レスポンシブ対応用
+const mobile = 0;
+const tablet = 1;
+const computer = 2;
+
 // collection.htmlへの画面遷移
 function transitionPage(type) {
     location.href = `collection.html?type=${ type }`;
@@ -38,4 +43,18 @@ function commonInit() {
         this.classList.toggle('close-btn');
         document.querySelector('header').classList.toggle('close-list');
     }, false);
+}
+
+// 操作端末取得
+function getUserTerminal() {
+    if (/Android|iPhone|iPod/i.test(navigator.userAgent)) {
+        return mobile;
+    }
+    else if (window.matchMedia('(max-width: 768px)').matches) {
+        return tablet;
+    }
+    else if (window.matchMedia('(min-width: 769px)').matches) {
+        return computer;
+    }
+    return '';
 }
